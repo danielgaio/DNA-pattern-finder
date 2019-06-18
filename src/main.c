e950af4c5e75685f03265ed197c20d21e9b80bba
danielgaio <gaio@danielgaio.com>
2019-06-18 19:43:12 -0300

    Falta iplementação da exibição da LCS

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#define tam_seq 101	// <<---- indicar num. caracteres nos arquivos
+#include <time.h>
+#define tam_seq 29	// <<---- indicar num. caracteres nos arquivos (+1 para o caractere de fim de string)
 
 // Considerações: seq"n" será o Texto, seq"n+1" será a Palavra a ser buscada no texto
 // Em sua versão básica a complexidade é O(N*M), sendo N o tamanho da palavra e M o tamanho do texto
@@ -16,8 +17,12 @@ int busca_subsequencia(char *, char *palavra);
 int count = 0;
 
 int main() {
-	char *nome_seq1 = "seq7-100.txt";	// <<---- _ indicar nomes arquivos
-	char *nome_seq2 = "seq8-100.txt";	// <<----			
+	// contagem do tempo
+	clock_t tInicio, tFim, tDecorrido;
+	tInicio = clock();
+
+	char *nome_seq1 = "seq1-10.txt";	// <<---- _ indicar nomes arquivos
+	char *nome_seq2 = "seq2-10.txt";	// <<----			
 	char palavra_aux[tam_seq];
 	int posicao;
 
@@ -46,30 +51,21 @@ int main() {
 
 	printf("Seq 2: %s\n\n", palavra);
 	//============= carrega sequencia 2 ===============
-
-
-
+	
 	//============= busca força bruta ===============
 	strcpy(palavra_aux, palavra);
 
 	// tenta combinações diminuindo a palavra a partir do fim
 	for(int i = 1; i < tam_seq; i++){
 		posicao = busca_subsequencia(texto, palavra_aux);
-		
 		if(posicao != -1){
-			printf("                                  	texto	: %s\n", texto);
+			printf("---------------------------------	texto	: %s\n", texto);
 			printf("iteração: %i - posição match: %i -	palavra	: %s\n", i, posicao+1, palavra_aux);
-			//sleep(1);
 		}
-		
-		
-
 		// remove ultimo caractere
 		palavra_aux[(strlen(palavra_aux)-1)] = '\0';
 	}
 	printf("\n");
-
-	//sleep(2);
 
 	strcpy(palavra_aux, texto);
 	char texto_aux[tam_seq];
@@ -80,46 +76,39 @@ int main() {
 		posicao = busca_subsequencia(texto_aux, palavra_aux);
 		
 		if(posicao != -1){
-			printf("                                  	texto	: %s\n", texto);
+			printf("---------------------------------	texto	: %s\n", texto);
 			printf("iteração: %i - posição match: %i - 	palavra	: %s\n", i, posicao+1, palavra_aux);
-			//sleep(1);
 		}
-
-		
-
 		// remove ultimo caractere
 		palavra_aux[(strlen(palavra_aux)-1)] = '\0';
 	}
-
-	
-
-	//sleep(2);
-	posicao = busca_subsequencia(texto, "ta");
-	printf("\n                   	texto	: %s\n", texto);
-	printf("posição match: %i -	palavra	: ta\n\n", posicao+1);
-	
-
-
-
-
-
 	//============= busca força bruta ===============
-
-
+	
+	/*
 	//============= busca por divisão e conquista ===============
 	int tam_texto = strlen(texto);
 	int tam_palavra = strlen(palavra);
 	printf("O tamanho da maior subsequencia comum eh: %i\n", tam_maior_subsequencia(texto, palavra, tam_texto, tam_palavra));
 	printf("A maior subsequencia eh: [não implementado]\n");
 	//============= busca por divisão e conquista ===============
+	*/
+
+	tFim = clock();
+	tDecorrido = ((tFim - tInicio) / (CLOCKS_PER_SEC / 1000));
+	printf("\n\nduração execução: %ld ms\n", tDecorrido);
 
 	return 0;
 }
 
 // função divisão e conquista
 int tam_maior_subsequencia( char *texto, char *palavra, int tam_texto, int tam_palavra ){
-    if (tam_texto == 0 || tam_palavra == 0) return 0;
+	// base de recurção
+    if (tam_texto == 0 || tam_palavra == 0){
+		printf("%c", palavra[tam_palavra-1]);
+		return 0;
+	}
     if (texto[tam_texto-1] == palavra[tam_palavra-1]){
+		
 		return 1 + tam_maior_subsequencia(texto, palavra, tam_texto-1, tam_palavra-1);
 	}else return max(tam_maior_subsequencia(texto, palavra, tam_texto, tam_palavra-1), tam_maior_subsequencia(texto, palavra, tam_texto-1, tam_palavra));
 }
